5340a8edc8fa0d7be3fe3baa936a65bf07820740
Felix Horger <felix.horger@kcl.ac.uk>
2021-12-30 23:43:22 +0100

    Improved, well, everything really. Added dependencies.

--- src/MRFingerprinting.jl
+++ src/MRFingerprinting.jl
@@ -2,66 +2,47 @@
 module MRFingerprinting
 
 	import Optim
-	using BlackBoxOptim
+	import BlackBoxOptim
+	import MRIEPG
+	using LinearAlgebra
+	using Statistics
 
-	@generated function global_optimisation_run(
-		fitfunc::Function,
-		measured_signal::AbstractArray{<: Number},
-		T1::Tuple{Real, Real},
-		T2::Tuple{Real, Real},
-		relB1::Union{Real, Tuple{Real, Real}}
-	)
-
-		if relB1 <: Real
-			search_range = :([R1, R2])
-			lower = :([R1[1], R2[1]])
-			upper = :([R1[2], R2[2]])
-			initial = :(0.5 .* [sum(R1), sum(R2)])
-			return_value = :(1 ./ result1, 1 ./ result2)
-		else
-			search_range = :([R1, R2, relB1])
-			lower = :([R1[1], R2[1], relB1[1]])
-			upper = :([R1[2], R2[2], relB1[2]])
-			initial = :(0.5 .* [sum(R1), sum(R2), sum(relB1)])
-			return_value = :(1 ./ result1[1:2], 1 ./ result2[1:2], result1[3], result2[3])
-		end
-
-		return quote
+	macro global_optimisation_run(search_range, lower, upper, initial)
+		esc(quote
 			# Put boundaries in Relaxometry
 			R1 = 1 ./ reverse(T1)
 			R2 = 1 ./ reverse(T2)
 
-			result1 = bboptimize(
-				fitfunc;
+			result_blackbox = BlackBoxOptim.bboptimize(
+				model;
 				SearchRange=$search_range,
 				MaxTime=10.0
 			)
-			result1 = best_candidate(result1)
+			result_blackbox = BlackBoxOptim.best_candidate(result_blackbox)
 
-			result2 = Optim.optimize(
-				fitfunc,
+			result_annealing = Optim.optimize(
+				model,
 				$lower,
 				$upper,
 				$initial,
 				Optim.SAMIN(),
 				Optim.Options(iterations=10^6)
 			)
-			result2 = Optim.minimizer(result2)
-			return $return_value
-		end
+			result_annealing = Optim.minimizer(result_annealing)
+		end)
 	end
 
 	function global_optimisation(
 		measured_signal::AbstractVector{<: Number},
 		T1::Tuple{Real, Real},
 		T2::Tuple{Real, Real},
-		relB1::Real;
+		relB1::Real,
 		# Parameters from here
+		cycles::Integer,
+		kmax::Integer,
 		α::AbstractVector{<: Real},
 		ϕ::AbstractVector{<: Real},
 		TR::Union{Tuple{Real, Real}, AbstractVector{<: Real}},
-		kmax::Integer,
-		cycles::Integer,
 		G::AbstractVector{<: Real},
 		τ::AbstractVector{<: Real},
 		D::Real
@@ -69,56 +50,79 @@ module MRFingerprinting
 		# Make to func::Function, parameters::Dict{String, Any}
 
 		α = relB1 .* α
-		ms = abs.(measured_signal) # This is bogus
-		fitfunc = R -> begin
-			s = abs.(driven_equilibrium(
-				α, ϕ, TR,
-				kmax, cycles,
-				G, τ, D,
-				R[1],
-				R[2]
-			)[1])
-			s ./= -norm(s)
-			s' * ms
+		model::Function = let measured_signal = abs.(measured_signal)'
+			θ -> begin
+				simulation = abs.(MRIEPG.driven_equilibrium(
+					kmax, cycles,
+					α, ϕ, TR,
+					G, τ, D,
+					(θ[1], θ[2])
+				)[1])
+				simulation ./= -norm(simulation)
+				measured_signal * simulation
+			end
 		end
 
-		results = global_optimisation_run(fitfunc, measured_signal, T1, T2, relB1)
-		return results
+		@global_optimisation_run(
+			[R1, R2], # Search range
+			[R1[1], R2[1]], # Lower
+			[R1[2], R2[2]], # Upper
+			[mean(R1), mean(R2)] # Initial parameters
+		)
+
+		return 1 ./ result_blackbox, 1 ./ result_annealing
 	end
 
 	function global_optimisation(
 		measured_signal::AbstractVector{<: Number},
 		T1::Tuple{Real, Real},
 		T2::Tuple{Real, Real},
-		relB1::Tuple{Real, Real};
+		relB1::Tuple{Real, Real},
 		# Parameters from here
+		cycles::Integer,
+		kmax::Integer,
 		α::AbstractVector{<: Real},
 		ϕ::AbstractVector{<: Real},
 		TR::Union{Tuple{Real, Real}, AbstractVector{<: Real}},
-		kmax::Integer,
-		cycles::Integer,
 		G::AbstractVector{<: Real},
 		τ::AbstractVector{<: Real},
 		D::Real
 	)
 		# Make to func::Function, parameters::Dict{String, Any}
+		# TODO: Macro for whole optimisation function, which eats up the "run"
+		# TODO: make model generated to make use of TR = Union...
 
-		ms = abs.(measured_signal) # This is bogus
-		fitfunc = R -> begin
-			s = abs.(driven_equilibrium(
-				R[3] .* α, ϕ, TR,
-				kmax, cycles,
-				G, τ, D,
-				R[1],
-				R[2]
-			)[1])
-			s ./= -norm(s)
-			s' * ms
+		model::Function = let measured_signal = abs.(measured_signal)'
+			θ -> begin
+				simulation = abs.(MRIEPG.driven_equilibrium(
+					kmax, cycles,
+					θ[3] .* α, ϕ, TR,
+					G, τ, D,
+					(θ[1], θ[2])
+				)[1])
+				simulation ./= -norm(simulation)
+				measured_signal * simulation
+			end
 		end
 
-		results = global_optimisation_run(fitfunc, measured_signal, T1, T2, relB1)
-		return results
+		@global_optimisation_run(
+			[R1, R2, relB1], # Search range
+			[R1[1], R2[1], relB1[1]], # Lower
+			[R1[2], R2[2], relB1[2]], # Upper
+			[mean(R1), mean(R2), mean(relB1)] # Initial parameters
+		)
+
+		return (
+			1 ./ result_blackbox[1:2],
+			1 ./ result_annealing[1:2],
+			result_blackbox[3],
+			result_annealing[3]
+		)
 	end
+
+
+
+
 
 	function eval_loss(
 		measured_signal::AbstractVector{<: Number},
