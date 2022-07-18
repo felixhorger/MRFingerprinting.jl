3344dd52d7d8426dfff39e06f878f35788063cc4
Felix Horger <felix.horger@kcl.ac.uk>
2022-07-18 16:47:02 +0100

    Cleaned up, low_rank_mask from indices, solved bug so that readouts can have a length > 1

--- src/low_rank.jl
+++ src/low_rank.jl
@@ -2,75 +2,66 @@
 # Definitions: Use flat spatial dimensions, i.e. vector is a matrix with one spatial and one temporal dimension.
 # For all the CG stuff however, this needs to be reshaped into a single vector
 
+"""
+	Useful for compression of actual kspace data for creating spatial ft without having to do the reshaping every time
+	Of course works for any time domain array
+"""
+function time2lr(xt::AbstractArray{<: Number, N}, V_conj::AbstractMatrix{<: Number}) where N
+	# TODO: This could be done with @turbo
+	shape = size(xt)
+	xt = reshape(xt, :, shape[N])
+	x = xt * V_conj
+	x = reshape(x, shape[1:N-1]..., size(V_conj, 2))
+	return x
+end
+"""
+	Sparse version of time2lr, so made for kspace only
+"""
 function kt2klr(
-	a::AbstractArray{<: Number, 3},
+	x::AbstractArray{<: Number, 3},
 	VH::AbstractMatrix{<: Number},
-	shape::NTuple{2, Integer},
-	indices::AbstractVector{<: Integer}...
-)
-	@assert all(size(a, 3) .== length.(indices))
-	@assert all(all(0 .< indices[d] .<= shape[d]) for d ∈ (1,2))
+	shape::NTuple{N, Integer},
+	indices::AbstractVector{<: NTuple{N, Integer}}
+) where N
+	@assert all(size(x, 3) .== length.(indices))
+	@assert all(all(0 .< indices[d] .<= shape[d]) for d ∈ 1:N)
 	timepoints = size(VH, 2)
-	b = zeros(ComplexF64, size(a, 1), shape..., size(a, 2), size(VH, 1)) # spatial dims..., channels, sigma
+	y = zeros(ComplexF64, size(x, 1), shape..., size(x, 2), size(VH, 1)) # spatial dims..., channels, sigma
 	# Must be zeros because if a location isn't sampled it would contain undefs
-	@inbounds for (i, j) in enumerate(zip(indices...))
+	@inbounds for (i, j) in enumerate(indices)
 		t = mod1(i, timepoints)
 		for σ in axes(VH, 1)
-			b[:, j..., :, σ] += VH[σ, t] * a[:, :, i]
+			y[:, j..., :, σ] += VH[σ, t] * x[:, :, i]
 		end
 	end
-	return b
-end
-# This should be used also in the function below
-
-
-function low_rank_mask(mask::AbstractArray{<: Number, N}, VH::AbstractMatrix{<: T}) where {T, N}
-	# TODO: take indices and not mask, then do "sparse" operations
-
-	# mask[time, space]
-	# VH[singular component, time]
-	#=
-		For each voxel, mask singular vectors in V,
-		then do inner product with vectors in VH to form one matrix per voxel,
-		transforming signals from the temporal low-rank domain into itself.
-	=#
-	shape = size(mask)[2:N]
-	lr_mask = Array{T}(undef, size(VH, 1), size(VH, 1), shape...)
-	V = VH'
-	@views for I in CartesianIndices(shape)
-		lr_mask[:, :, I] = VH * (mask[:, I] .* V)
-	end
-	return lr_mask
+	return y
 end
 
+
+"""
+	 Convenience for getting the more practical versions of V: V* and V^T
+"""
 function convenient_Vs(VH::AbstractMatrix{<: Number})
-	# Convenience for getting the more practical versions of V: V* and V^T
-	# TODO: collect or not?
 	collect(transpose(VH)), conj.(VH)
 end
 
 
 """
-	plan_lr2time(x::AbstractArray{<: Number, N}, V_conj::AbstractMatrix{<: Number}, VT::AbstractMatrix{<: Number}) where N
-
-(spatial dimensions..., singular components)
-
+	num_other is everything except from dynamic
 """
-function plan_lr2time(V_conj::AbstractMatrix{<: Number}, VT::AbstractMatrix{<: Number}, shape::NTuple{N, Integer}) where N
+function plan_lr2time(V_conj::AbstractMatrix{<: Number}, VT::AbstractMatrix{<: Number}, num_other::Integer)
 	# TODO: This could be done with @turbo
-	time, num_σ = size(V_conj)
-	@assert num_σ == shape[N]
-	residual_dimensions = prod(shape[1:N-1])
-	input_dimension = num_σ * residual_dimensions
-	output_dimension = time * residual_dimensions
+	num_time, num_σ = size(V_conj)
+	input_dimension = num_σ * num_other
+	output_dimension = num_time * num_other
 	Λ = LinearMap{ComplexF64}(
 		y -> begin
-			y = reshape(y, residual_dimensions, num_σ)
+			y = reshape(y, num_other, num_σ)
 			yt = y * VT
 			vec(yt)
 		end,
 		yt -> begin
-			yt = reshape(yt, residual_dimensions, time)
+			yt = reshape(yt, num_other, num_time)
 			y = yt * V_conj
 			vec(y)
 		end,
@@ -82,17 +73,6 @@ end
 
 
 
-"""
-	Useful for compression actual kspace data for creating spatial ft without having to do the reshaping every time
-"""
-function time2lr(xt::AbstractArray{<: Number, N}, V_conj::AbstractMatrix{<: Number}) where N
-	# TODO: This could be done with @turbo
-	shape = size(xt)
-	xt = reshape(xt, :, shape[N])
-	x = xt * V_conj
-	x = reshape(x, shape[1:N-1]..., size(V_conj, 2))
-	return x
-end
 
 
 
@@ -108,34 +88,68 @@ end
 @inline plan_lr2kt(Λ::LinearMap, F::LinearMap, S::LinearMap) = plan_lr2kt(Λ, F) * S
 
 
+"""
+	shape is the shape of the phase encoding plane
+	mask[time, space]
+	VH[singular component, time]
+	
+	For each voxel, mask singular vectors in V,
+	then do inner product with vectors in VH to form one matrix per voxel,
+	transforming signals from the temporal low-rank domain into itself.
+	
+	Note: There is no use case of difference phase encoding steps with overlapping readouts (k-wise) I know of,
+	so this assumes only the same phase encoding step overlaps with itself (to compute the mask).
+"""
+function low_rank_mask(
+	V_conj::AbstractMatrix{<: T},
+	VT::AbstractMatrix{<: T},
+	indices::AbstractVector{<: NTuple{N, Integer}},
+	shape::NTuple{N, Integer},
+	num_dynamic::Integer
+) where {N, T}
+	num_readouts = prod(shape)
+	num_σ = size(VT, 1)
+	linear_indices = LinearIndices(shape)
+	perm = sortperm(indices; by=(t::NTuple{N, Integer} -> linear_indices[t...]))
+	lr_mask = zeros(T, size(VT, 1), size(VT, 1), shape...)
+	for i in eachindex(perm)
+		j = perm[i]
+		x = indices[j]
+		dynamic = mod1(j, num_dynamic)
+		mixing_matrix = @view lr_mask[:, :, x...]
+		@turbo for σ1 = 1:num_σ, σ2 = 1:num_σ
+			mixing_matrix[σ1, σ2] += VT[σ1, dynamic] * V_conj[dynamic, σ2] # Outer product
+		end
+	end
+	return lr_mask
+end
 
 """
-
-	function apply_lr_mask(y::AbstractVector{C}, lr_mask::AbstractArray{R, 3}, channels::Integer) where {C <: Complex, R <: Real}
 
 lr_masks[σ, σ, spatial dimensions]
 
 """
 function apply_lr_mask(
 	y::AbstractVector{C},
-	lr_mask::AbstractArray{<: Real, 3},
+	lr_mask::AbstractArray{<: Real, 3}, # Flat spatial dimension
+	readout_length::Integer, # Needs to be separate because of the order in which y usually is
 	channels::Integer # Makes sense to use Val{N}?
 ) where C <: Complex
 	num_σ = size(lr_mask, 1)
 	# TODO: check shape
-	y = reshape(y, size(lr_mask, 3), channels, num_σ) # Reshapes allocate ...
+	y = reshape(y, readout_length, size(lr_mask, 3), channels, num_σ) # Reshapes allocate ...
 	yd = decomplexify(y)
 	ymd = similar(yd)
-	@turbo for x in axes(lr_mask, 3), c = 1:channels
+	@turbo for x in axes(lr_mask, 3), c = 1:channels, s = 1:readout_length
 		for σ2 = 1:num_σ
 			ym_real = 0.0
 			ym_imag = 0.0
 			for σ1 = 1:num_σ
-				ym_real += yd[1, x, c, σ1] * lr_mask[σ1, σ2, x]
-				ym_imag += yd[2, x, c, σ1] * lr_mask[σ1, σ2, x]
+				ym_real += yd[1, s, x, c, σ1] * lr_mask[σ1, σ2, x]
+				ym_imag += yd[2, s, x, c, σ1] * lr_mask[σ1, σ2, x]
 			end
-			ymd[1, x, c, σ2] = ym_real
-			ymd[2, x, c, σ2] = ym_imag
+			ymd[1, s, x, c, σ2] = ym_real
+			ymd[2, s, x, c, σ2] = ym_imag
 		end
 	end
 	ym = reinterpret(C, vec(ymd))
@@ -144,28 +158,29 @@ end
 function apply_lr_mask(
 	y::AbstractVector{C},
 	lr_mask_d::AbstractArray{<: Real, 4},
+	readout_length::Integer, # Needs to be separate because of the order in which y usually is
 	channels::Integer
 ) where C <: Complex
 	num_σ = size(lr_mask, 3)
-	y = reshape(y, size(lr_masks, 3), channels, num_σ)
+	y = reshape(y, readout_length, size(lr_masks, 3), channels, num_σ)
 	yd = decomplexify(y)
 	ymd = similar(yd) # y *m*asked and *d*ecomplexified
-	@turbo for x = axes(lr_mask, 3), c = 1:channels
+	@turbo for x = axes(lr_mask, 3), c = 1:channels, s = 1:readout_length
 		for σ2 = 1:num_σ
 			ym_real = 0.0
 			ym_imag = 0.0
 			for σ1 = 1:num_σ
 				ym_real += (
-					  yd[1, x, c, σ1] * lr_mask_d[1, σ1, σ2, x]
-					- yd[2, x, c, σ1] * lr_mask_d[2, σ1, σ2, x]
+					  yd[1, s, x, c, σ1] * lr_mask_d[1, σ1, σ2, x]
+					- yd[2, s, x, c, σ1] * lr_mask_d[2, σ1, σ2, x]
 				)
 				ym_imag += (
-					  yd[1, x, c, σ1] * lr_mask_d[1, σ1, σ2, x]
-					+ yd[2, x, c, σ1] * lr_mask_d[2, σ1, σ2, x]
+					  yd[1, s, x, c, σ1] * lr_mask_d[1, σ1, σ2, x]
+					+ yd[2, s, x, c, σ1] * lr_mask_d[2, σ1, σ2, x]
 				)
 			end
-			ymd[1, x, c, σ2] = ym_real
-			ymd[2, x, c, σ2] = ym_imag
+			ymd[1, s, x, c, σ2] = ym_real
+			ymd[2, s, x, c, σ2] = ym_imag
 		end
 	end
 	ym = reinterpret(C, vec(ymd))
@@ -175,30 +190,44 @@ end
 
 
 """
-	plan_lr_masking(lr_mask::AbstractArray{<: Number, N}, channels::Integer) where N
 
-sensitivites[spatial dimensions..., channels]
-lr_mask[singular components, singular components, spatial dimensions...]
-
+lr_mask[σ1, σ2, spatial dimensions...]
+lr_mask not copied
 """
-function plan_lr_masking(lr_mask::AbstractArray{<: Number, N}, channels::Integer) where N
-	shape = size(lr_mask)
-	spatial_shape = shape[3:N]
-	num_σ = shape[1]
-	@assert shape[2] == num_σ
-	spatial_dimensions = prod(spatial_shape)
-	lr_mask = reshape(lr_mask, num_σ, num_σ, spatial_dimensions) # It isn't copied, is that good? No...
+function plan_lr_masking(lr_mask::AbstractArray{<: Number, N}, readout_length::Integer, num_channels::Integer) where N
+	# Get and check shapes
+	lr_mask_shape = size(lr_mask)
+	num_σ = lr_mask_shape[1]
+	@assert lr_mask_shape[2] == num_σ
+	shape = lr_mask_shape[3:N]
+	num_phase_encode = prod(shape)
+	# Reshape and split real/imag
+	lr_mask = reshape(lr_mask, num_σ, num_σ, num_phase_encode) # It isn't copied
 	lr_mask_d = decomplexify(lr_mask)
-	dimension = spatial_dimensions * channels * num_σ
 	# Define function
 	M = LinearMap{ComplexF64}(
 		y::AbstractVector{<: Complex} -> begin
-			apply_lr_mask(y, lr_mask_d, channels)
+			apply_lr_mask(y, lr_mask_d, readout_length, num_channels)
 		end,
-		dimension,
+		num_phase_encode * readout_length * num_channels * num_σ,
 		ishermitian=true
 	)
 	return M
+end
+"""
+shape includes channels
+"""
+function plan_lr_masking(
+	V_conj::AbstractMatrix{<: Number},
+	VT::AbstractMatrix{<: Number},
+	indices::AbstractVector{<: NTuple{N, Integer}},
+	shape::NTuple{N, Integer},
+	readout_length::Integer,
+	num_channels::Integer,
+	num_dynamic::Integer
+) where N
+	lr_mask = low_rank_mask(V_conj, VT, indices, shape, num_dynamic)
+	return plan_lr_masking(lr_mask, readout_length, num_channels)
 end
 
 
@@ -207,30 +236,28 @@ end
 
 
 """
-	projection_matrix(x::AbstractArray{C, N}, DT_renorm::AbstractMatrix{<: Real}, matches::AbstractVector{<: Integer}) where {C <: Complex, N}
 
 x[spatial dimensions ..., singular components]
+
+`matches` must be preallocated and filled to update P
+DT_renorm[σ, fingerprints] must be normalised in the SVD domain after cut-off
 
 """
 function projection_matrix(
 	DT_renorm::AbstractMatrix{<: Real},
 	matches::AbstractVector{<: Integer},
-	shape::NTuple{N, Integer},
+	num_x::Integer,
+	num_σ::Integer,
 	T::Type{C}
-) where {C <: Complex, N}
-	# `matches` must be preallocated and filled to update P
-	# DT_renorm[σ, fingerprints] must be normalised in the SVD domain after cut-off
-	spatial_dimensions = prod(shape[1:N-1])
-	@assert length(matches) == spatial_dimensions
-	num_σ = shape[N]
+) where C <: Complex
+	@assert length(matches) == num_x
 	@assert size(DT_renorm, 1) == num_σ
-	dimension = spatial_dimensions * num_σ
 	num_D = size(DT_renorm, 2)
 
 	P = LinearMap{ComplexF64}(
 		x::AbstractVector{<: Complex} -> begin
-			@assert all(0 .<= matches .<= num_D) # Zero is a forbidden index, in that case a zero filled vector will be returned
-			x = reshape(x, spatial_dimensions, num_σ) # Weird: this has to be done before complexifying, otherwise the same error as in overlap!() with views
+			@assert all((i -> 0 <= i <= num_D), matches) # Zero is a forbidden index, in that case a zero filled vector will be returned
+			x = reshape(x, num_x, num_σ) # Weird: this has to be done before complexifying, otherwise the same error as in overlap!() with views
 			xd = decomplexify(x) # d for decomplexified
 			xpd = similar(xd)
 			for xi in eachindex(matches)
@@ -249,7 +276,7 @@ function projection_matrix(
 			xp = reinterpret(C, vec(xpd))
 			xp
 		end,
-		dimension,
+		num_x * num_σ,
 		ishermitian=true
 	)
 	return P
@@ -338,6 +365,4 @@ function admm(
 	end
 	return x, y
 end
-# TODO: Get tests from 20220401_Simulation
-
 
